a334f2ffaa9ff22301e304f6571a3aab169fef3c
Robert van Eijk <251185@virtualwindow.co.za>
2026-05-18 21:57:14 +0200

    Update App.jsx
    Updated App.jsx.
    
    npm run dev now works

--- creative-auth/src/App.jsx
+++ creative-auth/src/App.jsx
@@ -122,7 +122,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // import pages
-// import SignUp from "./Pages/SignUp";
+import SignUp from "./Pages/SignUp";
 import SignIn from "./Pages/SignIn";
 
 function App() {
@@ -132,7 +132,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Navigate replace to="/signin" />} />
 					<Route path="/signin" element={<SignIn />} />
-					{/* <Route path="/signup" element={<SignUp />} /> */}
+					<Route path="/signup" element={<SignUp />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
